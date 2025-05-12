ceb624032eaa4e5e477b4cf5ea51b63933916a46
23415av <23415av@hvhs.school.nz>
2025-05-12 21:57:56 +0000

    Started

---
+++ main.js
@@ -0,0 +1,18 @@
+/*************************
+Achlys
+15
+Week 3
+Term 2
+*************************/
+
+//Variables
+
+
+/*************************
+Main Code
+*************************/
+console.log("Well, you want a cd? Worry not, as long as you've answered the questions [which will appear if you press the button] then you only need to read a bit furthor");
+
+/*************************
+Functions
+*************************/
