bb6673ac42edb9938c97cd8f38e48670d2da716b
23415av <23415av@hvhs.school.nz>
2025-05-13 23:35:22 +0000

    May 13, 2025, 11:35 PM

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ Main Code
 *************************/
 console.log("Well, you want a cd? Worry not, as long as you've answered the questions [which will appear if you press the button] then you only need to read a bit furthor");
 
+
 /*************************
 Functions
 *************************/
