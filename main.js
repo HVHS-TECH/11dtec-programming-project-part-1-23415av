61dfcc7fd3b0ec9d6553de36b56261700c29332d
23415av <23415av@hvhs.school.nz>
2025-05-13 23:40:15 +0000

    May 13, 2025, 11:40 PM

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ Term 2
 *************************/
 
 //Variables
-
+var userName
 
 /*************************
 Main Code
 *************************/
-console.log("Well, you want a cd? Worry not, as long as you've answered the questions [which will appear if you press the button] then you only need to read a bit furthor");
+userName = askUserName();
+console.log("Well " +userName+ ". You want a cd? Worry not! As long as you've answered the questions [which will appear if you press the button] then you only need to read a bit furthor");
 
 
 /*************************
 Functions
 *************************/
+function askUserName() {
+    var temp1 = prompt("Before we start, I was hopping you could tell me your name down below");
+    return (temp1);
+}
