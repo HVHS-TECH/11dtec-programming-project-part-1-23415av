c7a378c1b2fb2b4f496456b12e47b4362f80042d
23415av <23415av@hvhs.school.nz>
2025-05-13 23:53:37 +0000

    May 13, 2025, 11:53 PM

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ Term 2
 *************************/
 
 //Variables
-var userName
+var userName;
+var cdArray = ["Everything is a Lot - Will Wood", "Eureka - Mother Mother", "Diamond Dogs - David Bowie", "Reputation - Taylor Swift", "Bad Hair Day - “Weird Al” Yankovic", "The Black Parade - My Chemical Romance"];
 
 /*************************
 Main Code
@@ -14,7 +15,18 @@ Main Code
 console.log("Well " +userName+ ". You want a cd? Worry not! As long as you've answered the questions [which will appear if you press the button] then you only need to read a bit furthor");
 userName = askUserName();
 function recomendation() {
-
+alert("So. To get your recomendation, I am going to tell you six genres and their corresponding number [0-5].");
+alert("Then a prompt will come up, telling you to enter the number of your desired genre.");
+alert("After that, all you need to do is open the console [ctrl-shift-i] and you'll see your recomendation!");
+alert("If you need to write them down, feel free to do so.");
+alert("Choose 0 if you prefer more obscure music.");
+alert("Choose 1 if you prefer something that's alternative.");
+alert("Choose 2 if you prefer something older compared to the rest.");
+alert("Choose 3 if you prefer pop music.");
+alert("Choose 4 if you prefer comedic and parody music.");
+alert("Choose 5 if you prefer something that falls under the emo category.");
+var choice = prompt("Now just enter the number");
+alert("To see what you got, look at the console [ctrl-shift-i].")
 }
 
 
